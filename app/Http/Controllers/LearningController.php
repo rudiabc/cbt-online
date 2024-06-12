e6a472f663741f82883341a92877eb10aa5949ff
Rudiansyah <rudiansya24678@gmail.com>
2024-06-12 11:25:43 +0700

    Create Route Project

---
+++ app/Http/Controllers/LearningController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LearningController extends Controller
+{
+    //
+
+    public function index(){
+        return 'hallo student, welcome and lets study';
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles, SoftDeletes;
+    use HasFactory, Notifiable, HasRoles;
 
 
     /**
